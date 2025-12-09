2c5ebb94911d15adc8d3059e6ed47027df810fa1
Yashwardhan Anand <yashwardhananand835@gmail.com>
2025-12-09 23:37:24 +0530

    Create bandname.py

---
+++ bandname.py
@@ -0,0 +1,5 @@
+print("hey there future rockstar!")
+name=input("which city did you grow up in?")
+pet_name=input("what's the name of your pet?")
+
+print("the name of your band is...... drumroll please "+name+" "+pet_name)
